0112b5cf92437133d49350afbbbf8d3a39178158
qwcan <ieatspamforbreakfasts1@gmail.com>
2026-02-21 00:45:39 -0600

    Comment

--- ChunkysFriendTweaks/PluginInfo.cs
+++ ChunkysFriendTweaks/PluginInfo.cs
@@ -2,5 +2,6 @@
 
 public class PluginInfo
 {
+    //111 is in front of the GUID so that it is (hopefully) loaded before any mods that use LethalLib/LLL
     public const string GUID = "111.ChunkysFriendTweaks";
 }
